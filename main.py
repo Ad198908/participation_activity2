0c4732c8c4391f7de4e914bfa9f01627206e9bab
Ad198908 <Agebretensay@student.cscc.edu>
2026-09-20 23:01:01 -0400

    Add participation_activity2

---
+++ main.py
@@ -0,0 +1,17 @@
+
+"""I want to show function format by using a variable, and for loop."""
+
+def Show_numbers():
+
+    numbers = [1, 3, 5, 7, 9] 
+
+    for number  in numbers:
+       
+        print(number)
+
+
+Show_numbers()
+  
+
+
+
